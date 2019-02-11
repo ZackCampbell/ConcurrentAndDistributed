8234a2bc7c4325062c25aa7b86a42f5389a4b589
Unknown <zack.c.campbell@gmail.com>
2019-02-10 18:03:47 -0600

    First Attempt at Q3, havent tested yet

--- src/HW2/PriorityQueue.java
+++ src/HW2/PriorityQueue.java
@@ -1,7 +1,21 @@
 package HW2;
 
+import java.util.LinkedList;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class PriorityQueue {
+    ReentrantLock queueLock = new ReentrantLock();
+    Condition notFull = queueLock.newCondition();
+    Condition notEmpty = queueLock.newCondition();
+
+    private LinkedList<Node> queue;
+    private int CAPACITY;
+    private int count = 0;
+    // 9 is highest priority and 0 is lowest
     public PriorityQueue(int capacity) {
+        queue = new LinkedList<>();
+        CAPACITY = capacity;
         // Creates a Priority queue with maximum allowed size as capacity
     }
 
@@ -10,21 +24,109 @@ public class PriorityQueue {
         // Returns the current position in the list where the name was inserted;
         // otherwise, returns -1 if the name is already present in the list.
         // This method blocks when the list is full.
-        return 0;
+        if (search(name) != -1)
+            return -1;
+        Node node = new Node(name, priority);
+        queueLock.lock();
+        try {
+            if (queue.isEmpty()) {
+                queue.add(node);
+                count++;
+                notEmpty.signal();
+                return 0;
+            }
+            while (count == CAPACITY) {
+                try {
+                    notFull.await();
+                } catch (InterruptedException e) {}
+            }
+            Node prev = queue.get(0), curr = null;
+            try {
+                prev.lock.lock();
+                curr = prev.next;
+                curr.lock.lock();
+                while (curr.next != null) {
+                    if (prev.priority <= priority && curr.priority > priority) {
+                        queue.add(queue.indexOf(curr), node);
+                        count++;
+                        notEmpty.signal();
+                        return queue.indexOf(node);
+                    }
+                    prev.lock.unlock();
+                    prev = curr;
+                    curr = curr.next;
+                    curr.lock.lock();
+                }
+                queue.add(node);
+                count++;
+                notEmpty.signal();
+                return queue.indexOf(node);
+            } finally {
+                curr.lock.unlock();
+                prev.lock.unlock();
+            }
+        } finally {
+            queueLock.unlock();
+        }
     }
 
     public int search(String name) {
+        if (queue.isEmpty())
+            return -1;
+        Node prev = queue.get(0), curr = null;
+        try {
+            prev.lock.lock();
+            curr = prev.next;
+            curr.lock.lock();
+            while (curr.next != null) {
+                if (curr.name.equals(name))
+                    return queue.indexOf(curr);
+                prev.lock.unlock();
+                prev = curr;
+                curr = curr.next;
+                curr.lock.lock();
+            }
+            if (curr.name.equals(name))         // Name is the last element in the queue
+                return queue.indexOf(curr);
+            return -1;                          // Name not found
+        } finally {
+            curr.lock.unlock();
+            prev.lock.unlock();
+        }
         // Returns the position of the name in the list;
         // otherwise, returns -1 if the name is not found.
-        return 0;
     }
 
     public String getFirst() {
+        queueLock.lock();
+        try {
+            while (count == 0) {
+                try {
+                    notEmpty.await();
+                } catch (InterruptedException e) {}
+            }
+            count--;
+            notFull.signal();
+            return queue.poll().name;
+        } finally {
+            queueLock.unlock();
+        }
         // Retrieves and removes the name with the highest priority in the list,
         // or blocks the thread if the list is empty.
-        return "";
     }
 
+    class Node {
+        ReentrantLock lock = new ReentrantLock();
+        String name;
+        int priority;
+        Node next;
 
+        public Node(String name, int priority) {
+            this.name = name;
+            this.priority = priority;
+            this.next = null;
+        }
+
+    }
 
 }
