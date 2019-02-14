de67e099314f8635156f386c8d44d1c6088d248d
[kazuhiro213] <[kazuhiro.aoyama@utexas.edu]>
2019-02-13 22:07:48 -0600

    CyclicBarrier.java works now i think

--- src/HW2/CyclicBarrier.java
+++ src/HW2/CyclicBarrier.java
@@ -1,29 +1,60 @@
-package HW2;
-
+/*
+ * EID's of group members
+ *
+ */
 import java.util.concurrent.Semaphore; // for implementation using Semaphores
 
 public class CyclicBarrier {
-    // Implement using semaphores
 
-    public CyclicBarrier(int parties) {
-        // Creates a new CyclicBarrier that will release threads only when
-        // the given number of threads are waiting upon it
-    }
-
-    int await() throws InterruptedException {
-        // Waits until all parties have invoked await on this CyclicBarrier.
-        // If the current thread is not the last to arrive then it is
-        // disabled for thread scheduling purposes and lies dormant until
-        // the last thread arrives.
-        // Returns: the arrival index of the current thread, where index
-        // (parties - 1) indicates the first to arrive and zero indicates
-        // the last to arrive.
-        
-        int index = 0;
-		
-          // you need to write this code
-	    return index;
-    }
+	private int parties;
+	private int index;
+	private Semaphore sem1, sem2, sem3;
 
 
+	public CyclicBarrier(int parties) {
+		this.parties = parties;
+		this.index= parties-1;
+		this.sem1 = new Semaphore(1);
+		this.sem2 = new Semaphore(0);
+		this.sem3 = new Semaphore(0);
+
+	}
+
+	public int await() throws InterruptedException {
+		int threadindex = 0;
+
+		// to assign index to each thread
+		sem1.acquire();
+		threadindex = index;
+		index--;
+		sem1.release();
+
+		// decide whether to release the thread
+		if(threadindex == 0){
+
+			for(int i = 0 ; i < parties - 1; i++)
+				sem2.release();
+
+		}else{
+			sem2.acquire();
+		}
+
+
+		// change waiting threads number
+		sem1.acquire();
+		index++;
+		sem1.release();
+
+		if(index == parties - 1 ){
+
+			for(int i = 0 ; i < parties - 1; i++)
+				sem3.release();
+
+		}else{
+
+			sem3.acquire();
+		}
+
+		return threadindex;
+	}
 }
