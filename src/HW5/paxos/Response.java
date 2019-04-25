13ecda7015a2e9511feb83e54bb85a29c1330fe7
kazuhiro213 <kazuhiro.aoyama@utexas.edu>
2019-04-24 20:38:34 -0500

    Added Constructor in Response.java

--- src/HW5/paxos/Response.java
+++ src/HW5/paxos/Response.java
@@ -9,7 +9,25 @@ import java.io.Serializable;
 public class Response implements Serializable {
     static final long serialVersionUID=2L;
     // your data here
+    int seq;
+    int prepare;
+    int reqNumber;
+    int oldReqNumber; // n a (highest accept request # seen)
+    Object oldValue;  // v a (highest accept object seen)
+    boolean accept;
+    Object value;
 
+    public Response(){
+        this.accept = false;
+    }
+
+    public Response(int seq, Object value, boolean accept, int oldReqNumber, Object oldValue){
+        this.seq = seq;
+        this.value = value;
+        this.accept = accept;
+        this.oldReqNumber = oldReqNumber;
+        this.oldValue = oldValue;
+    }
 
     // Your constructor and methods here
 }
